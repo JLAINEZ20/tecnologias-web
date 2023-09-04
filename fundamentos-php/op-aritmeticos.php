c88c1323cfdf8ba42c199d641ae008831da0e02c
JLAINEZ20 <laguinagajoselu@uss.edu.pe>
2023-09-04 15:19:42 -0500

    operadores

---
+++ fundamentos-php/op-aritmeticos.php
@@ -0,0 +1,18 @@
+<?php
+$numero=10;
+$numero2=50;
+
+$suma=$numero+$numero2;
+$resta=$numero-$numero2;
+$mult=$numero*$numero2;
+$div=$numero/$numero2;
+
+//imprimir los resultados
+echo "Suma: ".$suma;
+echo "<br>";
+echo "Resta: ".$resta;
+echo "<br>";
+echo "Multiplicacion: ".$mult;
+echo "<br>";
+echo "Division: ".$div;
+?>
