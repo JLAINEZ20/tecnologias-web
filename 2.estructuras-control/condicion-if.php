67218efe1de9a2191cbe36a171c5ec2f96102a96
JLAINEZ20 <laguinagajoselu@uss.edu.pe>
2023-09-04 15:46:09 -0500

    condicionales

---
+++ 2.estructuras-control/condicion-if.php
@@ -0,0 +1,9 @@
+<?php
+$edad=20;
+
+if($edad>=18){
+echo "Mayor de edad";
+}else{
+    echo "Menor edad;"
+}
+?>
---
+++ 2.estructuras-control/selectiva-switch.php
@@ -0,0 +1,30 @@
+<?php
+    $numero=2;
+    switch($numero){
+        case 1:
+            echo "Lunes";
+            break;
+        case 2:
+            echo "Martes";
+            break;
+        case 3:
+            echo "Miercoles";
+            break;
+        case 4:
+            echo "Jueves";
+            break;
+        case 5:
+            echo "Viernes";
+            break;
+        case 6:
+            echo "Sabado";
+            break;
+        case 6:
+            echo "Domingo";
+            break;
+        default:
+            echo "Ingrese un valor de 1 a 7";
+            break;
+
+    }
+?>
